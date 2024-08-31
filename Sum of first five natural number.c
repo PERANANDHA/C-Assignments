70ad51a8761cec1eb037466b015d648e5bf94f87
PERANANDHA K L <153747209+PERANANDHA@users.noreply.github.com>
2024-08-31 20:59:28 +0530

    Sum of 1st five natural number.c
    C++ Program to print sum of 1st five natural numbers

---
+++ Sum of first five natural number.c
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    for(int i=0;i<=5;i++){
+        sum+=i;
+    }
+    cout<<"The sum of first five natural number is: "<<sum;
+    return 0;
+}
