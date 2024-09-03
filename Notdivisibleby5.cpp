42a0856129abddd0a52e0d6f94cda68607f492b0
PERANANDHA K L <153747209+PERANANDHA@users.noreply.github.com>
2024-09-03 22:43:18 +0530

    Notdivisibleby5.cpp

---
+++ Notdivisibleby5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num[6];
+    cout<<"Enter Six Numbers: ";
+    for(int i=0;i<6;i++){
+        cin>>num[i];
+    }
+    for(int i=0;i<6;i++){
+        if(num[i]%5==0){
+            continue;
+        }
+        else{
+            cout<<"The numbers are not divisible by 5 is "<<num[i]<<endl;
+        }
+    }
+    return 0;
+}
