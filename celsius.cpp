8813cdaa112f9dfa985a68c235dbc3a41fa825f0
PERANANDHA K L <153747209+PERANANDHA@users.noreply.github.com>
2024-09-11 21:16:26 +0530

    celsius.cpp
    Write a C++ program to convert a temperature from Celsius to Fahrenheit.

---
+++ celsius.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int fahrenheit,celsius;
+    cout<<"Enter the celsius: ";
+    cin>>celsius;
+    fahrenheit=celsius*(9/5)+32;
+    cout<<"The fahrenheit is "<<fahrenheit;
+    return 0;
+}
